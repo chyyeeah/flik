46bc780d2019db805adc7c435bddefcb726964a0
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-30 11:33:07 -0700

    additional setup

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/index.jsx',
+
+  output: {
+    path: path.resolve('client/dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        },
+      },
+    ],
+  },
+};
