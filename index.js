cc0d72f2cfec4835901902df698e992a2c0bb9fa
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-30 17:18:36 -0700

    reorganized server code

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-app.use(express.static(path.resolve('client/dist')));
-
-const PORT = 1337;
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
