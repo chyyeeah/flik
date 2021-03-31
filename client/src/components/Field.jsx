cc4e8eb95f1d95731039547612072ae9c62d4b17
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-30 17:23:22 -0700

    forgot to do incremental commits - added logic to randomly place a target - added logic to remove the target when a click has occurred - added logic to handle the set user modal (input and display) - added logic to log the click distance away from target as well as duration to click - added logic to post the score to the server - added conditionally render for the capture layer and set user modal

--- client/src/components/Field.jsx
+++ client/src/components/Field.jsx
@@ -1,47 +1,82 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import calcDistance from '../utils/calcDistance';
+import SetUser from './SetUser.jsx';
 
 export default (props) => {
   const [ windowDimensions, setWindowDimensions ] = useState({
     width: window.innerWidth, height: window.innerHeight
   });
+  const [ username, setUsername ] = useState('');
+  const [ usernameModal, setUsernameModal ] = useState(true);
   const [ gameInProgress, setGameInProgress ] = useState(false);
   const [ targetsRemaining, setTargetsRemaining ] = useState(0);
-  const [ targetStatus, setTargetStatus ] = useState({
+  const [ targetVisible, setTargetVisible ] = useState(false);
+  const [ targetPosition, setTargetPosition ] = useState({
     width: '0',
     height: '0',
     top: '0',
     left: '0',
+    backgroundColor: 'transparent'
   });
+  const [ startTime, setStartTime ] = useState(0);
+  const [ results, setResults ] = useState([]);
 
   useEffect(() => {
     if (targetsRemaining < 1) {
-      setGameInProgress(false);
+      if (gameInProgress) {
+        setGameInProgress(false);
+        axios.post('/score', results)
+          .then(() => {
+            setResults([]);
+          });
+      }
     } else {
-      // pop up target
-      const posX = Math.floor(Math.random() * windowDimensions.width);
-      const posY = Math.floor(Math.random() * windowDimensions.height);
-      setTargetStatus({
-        width: '30px',
-        height: '30px',
-        top: `${posY}px`,
-        left: `${posX}px`,
-      });
+      setTimeout(() => {
+        const posX = Math.floor(Math.random() * windowDimensions.width);
+        const posY = Math.floor(Math.random() * windowDimensions.height);
+        setStartTime(new Date().getTime());
+        setTargetPosition({
+          width: '30px',
+          height: '30px',
+          top: `${posY}px`,
+          left: `${posX}px`,
+          backgroundColor: 'red'
+        });
+        setTargetVisible(true);
+      }, Math.random() * 1000);
     }
-  }, [ gameInProgress, targetsRemaining ]);
+  }, [ targetsRemaining ]);
 
+  const inputHandler = e => setUsername(e.target.value);
+  const submitHandler = e => {
+    e.preventDefault();
+    setUsernameModal(false);
+  };
   let timer;
   const countdown = () => timer = setTimeout(() => {
     setTargetsRemaining(1);
     setGameInProgress(true);
-  }, 3000);
+  }, 2000);
   const abortStart = () => clearTimeout(timer);
-  const clickAttempt = () => {
+  const clickAttempt = (e) => {
+    const distance = calcDistance(
+      targetPosition.left,
+      targetPosition.top,
+      e.pageX,
+      e.pageY,
+    );
+    const duration = new Date().getTime() - startTime;
+
+    setResults([...results, { distance_from_target: distance, click_time: duration }]);
+    setTargetVisible(false);
     setTargetsRemaining(targetsRemaining - 1);
-    setTargetStatus({
+    setTargetPosition({
       width: '0',
       height: '0',
       top: `0`,
       left: `0`,
+      backgroundColor: 'transparent'
     });
   };
 
@@ -52,15 +87,27 @@ export default (props) => {
         onMouseLeave={abortStart} />
     : null;
 
+  const captureLayer = gameInProgress && targetVisible
+    ? <div
+      id='capture-layer'
+      onClick={clickAttempt}></div>
+    : null;
+
+  const setUserModal = usernameModal
+    ? <SetUser
+        username={username}
+        inputHandler={inputHandler}
+        submitHandler={submitHandler} />
+    : null;
+
   return (
     <div>
       { startButton }
-      <div
-        id='capture-layer'
-        onClick={clickAttempt}></div>
+      { captureLayer }
       <button
         className='target'
-        style={targetStatus}></button>
+        style={targetPosition}></button>
+      { setUserModal }
     </div>
   );
 };
