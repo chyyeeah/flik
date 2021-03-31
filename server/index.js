99d2749808d405ed6f8665e434c77ed2e8dccc29
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-30 22:18:25 -0700

    added morgan middleware

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan');
 const { getAllScores, addScore } = require('./controller/index.js');
 
 const app = express();
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static(path.resolve('client/dist')));
 app.use(bodyParser.json());
