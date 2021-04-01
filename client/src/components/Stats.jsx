8581b775468936a1352aac4f74da52bc5643bf64
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-31 20:21:10 -0700

    added 2 overlapping graphs: reaction time and distance from target

--- client/src/components/Stats.jsx
+++ client/src/components/Stats.jsx
@@ -1,30 +1,86 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { ResponsiveLine } from 'nivo';
 
-const data = [{
-  "id": "test",
-  "data": [
-    {
-      "x": "2021-03-27",
-      "y": 90
-    },
-    {
-      "x": "2021-03-28",
-      "y": 100
-    },
-    {
-      "x": "2021-03-29",
-      "y": 105
-    },
-    {
-      "x": "2021-03-30",
-      "y": 200
-    },
-  ]
-}];
+export default ({ username }) => {
+  const [ distanceData, setDistanceData ] = useState([]);
+  const [ durationData, setDurationData ] = useState([]);
 
+  useEffect(() => {
+    axios.get(`/stats/${username}`)
+      .then(res => {
+        console.log(res);
+        setDistanceData(res.data.resultDistance);
+        setDurationData(res.data.resultDuration);
+      });
+  }, []);
 
-export default () => (
-  <ResponsiveLine
-    data={data} />
-);
+  const statsGraph = distanceData.length > 0 && durationData.length > 0
+    ? <>
+        <div className='distance-graph'>
+          <ResponsiveLine
+            data={distanceData}
+            theme={{
+              axis: {
+                legend: {
+                  text: {
+                    fill: 'green'
+                  }
+                }
+              }
+            }}
+            colors={['green']}
+            curve="natural"
+            pointSize={12}
+            enablePointLabel={true}
+            layers={['axes', 'lines', 'markers', 'legends']}
+            axisLeft={{
+              legend: 'Distance From Target (px)',
+              legendPosition: 'center',
+              legendOffset: -40
+            }}
+            margin={{ top: 50, right: 40, bottom: 80, left: 50 }} />
+        </div>
+        <div className='duration-graph'>
+          <ResponsiveLine
+            data={durationData}
+            theme={{
+              axis: {
+                // ticks: {
+                //   line: {
+                //     stroke: color
+                //   },
+                //   text: { fill: color }
+                // },
+                legend: {
+                  text: {
+                    fill: 'red'
+                  }
+                }
+              }
+            }}
+            colors={['red']}
+            curve="natural"
+            pointSize={12}
+            enablePointLabel={true}
+            layers={['lines', 'markers']}
+            enableGridY={false}
+            enableGridX={false}
+            axisLeft={null}
+            axisBottom={null}
+            axisRight={{
+              legend: 'Reaction Time (ms)',
+              legendPosition: 'center',
+              legendOffset: 60
+            }}
+            margin={{ top: 50, right: 50, bottom: 80, left: 50 }} />
+        </div>
+      </>
+    : null;
+
+  return (
+    <div className="wrapper">
+      { statsGraph }
+    </div>
+  );
+};
