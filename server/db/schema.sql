edacb18738f1bdb83e4a3f041d502a54c57e84a8
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-31 20:18:00 -0700

    added queries for inserting dummy data

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -11,3 +11,15 @@ CREATE TABLE scores (
 -- VALUES ('2021-3-30', 'wilson', 892734, 15.12394);
 -- INSERT INTO scores (created_date, player, click_time, distance_from_target)
 -- VALUES ('2021-3-30', 'billy bob', 2734, 5.12394);
+-- INSERT INTO scores (created_date, player, click_time, distance_from_target)
+-- VALUES ('2021-3-25', 'billy bob', 872234, 51.12394);
+-- INSERT INTO scores (created_date, player, click_time, distance_from_target)
+-- VALUES ('2021-3-25', 'billy bob', 452734, 50.23);
+-- INSERT INTO scores (created_date, player, click_time, distance_from_target)
+-- VALUES ('2021-3-27', 'billy bob', 21734, 45.194);
+-- INSERT INTO scores (created_date, player, click_time, distance_from_target)
+-- VALUES ('2021-3-28', 'billy bob', 12734, 30.12394);
+-- INSERT INTO scores (created_date, player, click_time, distance_from_target)
+-- VALUES ('2021-3-29', 'billy bob', 8734, 10.12394);
+-- INSERT INTO scores (created_date, player, click_time, distance_from_target)
+-- VALUES ('2021-1-29', 'billy bob', 812734, 500.12394);
