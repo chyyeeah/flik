73db3bde3a11bf0e644fb330c589d5cef36c613d
Wilson Cheah <wilsoncheah5@gmail.com>
2021-03-31 20:19:29 -0700

    added query to select stats for a given user

--- server/model/index.js
+++ server/model/index.js
@@ -1,8 +1,16 @@
+const moment = require('moment');
 const db = require('../db/index.js');
 
-module.exports.getAllScores = () => db`
-  SELECT *
+module.exports.selectStats = (username) => db`
+  SELECT
+    created_date,
+    AVG(distance_from_target) AS avgDistance,
+    AVG(click_time) AS avgDuration
   FROM scores
+  WHERE player = ${username}
+    AND created_date > ${new Date(moment().subtract(14, 'days'))}
+  GROUP BY created_date
+  ORDER BY created_date ASC
 `;
 
 module.exports.insertScore = (date, player, distance, duration) => db`
