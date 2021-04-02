4f70130bf002c97e3f1ae3d241f8f0e20d766a05
Wilson Cheah <wilsoncheah5@gmail.com>
2021-04-01 17:48:58 -0700

    added bulma styling (columns and modal) performs and API call for the users career stats and displays them

--- client/src/components/HubModal.jsx
+++ client/src/components/HubModal.jsx
@@ -1,11 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Stats from './stats.jsx';
 
-export default ({ username, setDisplayHubModal }) => {
+export default ({
+  username, setDisplayHubModal,
+  results, setResults }) => {
+  const  [ careerStats, setCareerStats ] = useState({});
+
+  useEffect(() => {
+    axios.get(`/stats/${username}/career`)
+      .then(res => {
+        setCareerStats(res.data);
+      })
+  }, []);
+
   const playClickHandler = e => {
     e.preventDefault();
     setDisplayHubModal(false);
+    setResults([]);
   };
+
+  const careerStatsResults = Object.entries(careerStats).length > 0
+    ? <div>
+        <h2 className='title is-2'>Career Stats</h2>
+        <div className="columns">
+          <div className="column">
+            <h4 className='title is-4'>Distance From Target Center</h4>
+            <p>{careerStats.avgcareerdistance.toFixed(3)} px</p>
+          </div>
+          <div className="column">
+            <h4 className='title is-4'>Reaction Time</h4>
+            <p>{careerStats.avgcareerduration.toFixed(3)} ms</p>
+          </div>
+        </div>
+      </div>
+    : null;
+
+  const latestResults = results.length > 0
+    ? <div>
+        <h3 className='title is-3'>Nice! Here are your results:</h3>
+        <div className='columns'>
+          <div className='column'>
+            <h4 className='title is-4'>Average Distance From Target Center</h4>
+            <p>{results[0].distance_from_target.toFixed(3)} px</p>
+          </div>
+          <div className='column'>
+            <h4 className='title is-4'>Average Reaction Time</h4>
+            <p>{results[0].click_time} ms</p>
+          </div>
+        </div>
+      </div>
+    : null;
 
   return (
     <div className="modal is-active">
@@ -15,7 +60,10 @@ export default ({ username, setDisplayHubModal }) => {
           <p className="modal-card-title">Hello, {username}!</p>
         </header>
         <section className="modal-card-body">
+          { careerStatsResults }
+          <h3 className='title is-3 mt-5'>7-Day Progress</h3>
           <Stats username={username} />
+          { latestResults }
         </section>
         <footer className="modal-card-foot">
           <button className="button is-success" onClick={playClickHandler}>Play?</button>
